098f1820f6b370c70279cccd36aea924d34bb915
Anton Kolonin <akolonin@gmail.com>
2022-03-19 23:20:53 +0700

    exolore freedoms

--- pygents/plot.py
+++ pygents/plot.py
@@ -8,6 +8,21 @@ def plot_profile(df,text,h=3,title=None):
     if title is not None:
         plt.title(title)
 
+def plot_bar(df,labels,values,title=None):
+    plt.rcParams["figure.figsize"] = (20,round(len(df)/5))
+    p = df[[labels,values]].plot.barh(x=labels); p.invert_yaxis();
+    if title is not None:
+        plt.title(title)
+
+def plot_dict(dic,labels,values,title=None,head=None):
+    df = pd.DataFrame([(key, dic[key]) for key in dic],columns=[labels,values])
+    df.sort_values(values,ascending=False,inplace=True)
+    if head is not None:
+        df = df[:head]
+    plt.rcParams["figure.figsize"] = (20,round(len(df)/5))
+    p = df[[labels,values]].plot.barh(x=labels); p.invert_yaxis();
+    if title is not None:
+        plt.title(title)
 
 def plot_profile_probabilities(counters,text,max_n,plot=True,debug=False):
     df = pd.DataFrame(profile_probabilities(counters,text,max_n,debug=debug),columns=['pos','char','p+','p-'])
@@ -19,7 +34,6 @@ def plot_profile_probabilities(counters,text,max_n,plot=True,debug=False):
         plot_profile( df[['char','p-']],text)
     return df
 
-
 def plot_profile_freedoms(model,text,max_n,plot=True,debug=False):
     df = pd.DataFrame(profile_freedoms(model,text,max_n,debug=debug),columns=['pos','char','f+','f-'])
     #print(df)
@@ -29,3 +43,20 @@ def plot_profile_freedoms(model,text,max_n,plot=True,debug=False):
         plot_profile( df[['char','f+']],text)
         plot_profile( df[['char','f-']],text)
     return df
+
+def plot_profile_avg_freedom(model,text,n_min,n_max,col):
+    sdf = None
+    for n in range(1,7):
+        plt.rcParams["figure.figsize"] = (20,2)
+        df = profile_freedoms_df(model,text,n,debug=False)
+        df[col] = df[col] / df[col].max()
+        plot_profile(df[['char',col]],text,title=str(n)+col)
+        if sdf is None:
+            sdf = df[['char',col]]
+        else:
+            sdf[col] = sdf[col] + df[col]
+
+    sdf[c] = sdf[col] / sdf[col].max()
+    plot_profile(sdf[['char',col]],text,title='1-7 '+col)
+    
+
