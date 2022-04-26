9a0c0181ea1158ea16fef06c874e31c9f9f5c812
Anton Kolonin <akolonin@gmail.com>
2022-04-26 12:04:09 +0700

    fix ddf- ddf+ testing script

---
+++ pygents/token_plot.py
@@ -0,0 +1,18 @@
+from pygents.token import *
+from pygents.util import list2matrix 
+from pygents.plot import matrix_plot 
+
+
+def evaluate_freedom_tokenizer_options(test_texts,ref_tokenizer,tokenizer,ngram_params,thresholds,plot=True,title=None,debug=False):
+    rlist = []
+    for nlist in ngram_params:
+        for threshold in thresholds: 
+            tokenizer.set_options(nlist = nlist, threshold=threshold)
+            avg_f1 = evaluate_tokenizer_f1(test_texts,ref_tokenizer,tokenizer,debug=False)
+            if debug:
+                print(nlist,threshold,avg_f1)
+            rlist.append((nlist,threshold,avg_f1))
+    if plot:
+        r,c,m = list2matrix(rlist)
+        matrix_plot(r,c,m,1.0,title,vmin=0.0) 
+
