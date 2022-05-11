5b7e7ac184b98bad34a146d56824e9f0af8924f6
Anton Kolonin <akolonin@gmail.com>
2022-05-11 19:18:29 +0700

    graph code into module

--- pygents/plot.py
+++ pygents/plot.py
@@ -84,3 +84,93 @@ def plot_profile_avg_freedom(model,text,n_min,n_max,col):
 
     sdf[col] = sdf[col] / sdf[col].max()
     plot_profile(sdf[['char',col]],text,title='1-7 '+col)
+    
+    
+# graph plots
+
+# https://plotly.com/python/tree-plots/
+# https://igraph.org/python/doc/tutorial/tutorial.html
+
+import igraph
+from igraph import Graph, EdgeSeq
+import plotly.graph_objects as go
+
+def make_annotations(pos, text, M, font_size=10, font_color='rgb(250,250,250)'):
+    L=len(pos)
+    if len(text)!=L:
+        raise ValueError('The lists pos and text must have the same len')
+    annotations = []
+    for k in range(L):
+        annotations.append(
+            dict(
+                text=text[k], # or replace labels with a different list for the text within the circle
+                x=pos[k][0], y=2*M-pos[k][1],
+                xref='x1', yref='y1',
+                font=dict(color=font_color, size=font_size),
+                showarrow=False)
+        )
+    return annotations
+
+def igraph_draw(labels,edges,title):
+    #https://igraph.org/python/doc/tutorial/tutorial.html
+    G = Graph()
+    nr_vertices = len(labels)
+    G.add_vertices(nr_vertices)
+    G.add_edges(edges)
+
+#    lay = G.layout('rt')
+    lay = G.layout('tree')
+
+    position = {k: lay[k] for k in range(nr_vertices)}
+    Y = [lay[k][1] for k in range(nr_vertices)]
+    M = max(Y)
+
+    es = EdgeSeq(G) # sequence of edges
+    E = [e.tuple for e in G.es] # list of edges
+
+    L = len(position)
+    Xn = [position[k][0] for k in range(L)]
+    Yn = [2*M-position[k][1] for k in range(L)]
+    Xe = []
+    Ye = []
+    for edge in E:
+        Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+        Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+    fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle-dot',
+                                size=18,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+    axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+    fig.update_layout(title=title,
+              annotations=make_annotations(position, labels, M),
+              font_size=12,
+              showlegend=False,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+    fig.show()
