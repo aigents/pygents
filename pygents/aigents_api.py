ede89e7ff543671311ab99c62f3e268fa6cabbaa
Anton Kolonin <akolonin@gmail.com>
2023-09-09 14:56:39 +0700

    Aigents sentiment API added

--- pygents/aigents_api.py
+++ pygents/aigents_api.py
@@ -1,6 +1,6 @@
 # MIT License
 # 
-# Copyright (c) 2015-2025 Aigents®, Anton Kolonin 
+# Copyright (c) 2015-2023 Aigents®, Anton Kolonin 
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
