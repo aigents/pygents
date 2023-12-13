59480b94ccdd03ec9c46711d446f27f4726e7d1a
Anton Kolonin <akolonin@gmail.com>
2023-12-13 23:25:05 +0700

    add class for generc metrics

--- pygents/aigents_api.py
+++ pygents/aigents_api.py
@@ -21,9 +21,6 @@
 # SOFTWARE.
 from _ast import arg
 
-"""
-Service wrapper around Aigents Java-based Web Service
-"""        
 
 import requests
 import urllib.parse
@@ -32,80 +29,84 @@ import json
 import re
 import emoji
 from pygents.text import url_lines
+from pygents.util import dictcount
 
 
 import logging
-logger = logging.getLogger(__name__)	
+logger = logging.getLogger(__name__)    
 
+"""
+Service wrapper around Aigents Java-based Web Service
+"""        
 class AigentsAPI:
 
-	def __init__(self, base_url='https://aigents.com/al', login_email='john@doe.org', secret_question='password', secret_answer='1234567', real_mode=True, name='Aigents', verbose=False):
-		self.name = name
-		self.verbose = verbose
-		self.base_url = base_url # Aigents Web API hosting URL
-		self.login_email = login_email # Aigents user identification by email 
-		self.secret_question = secret_question # Aigents prompt for password
-		self.secret_answer = secret_answer # Aigents password value
-		self.real_mode = real_mode # whether to connect to real Aigents server (True) or fake test oe (False) 
-		if self.verbose:
-			logger.info('Creating Aigents session')
-		#print(self)
-		self.create_session()
-	
-	def __del__(self):
-		if self.verbose:
-			logger.info('Closing Aigents session')
-		self.close_session()
+    def __init__(self, base_url='https://aigents.com/al', login_email='john@doe.org', secret_question='password', secret_answer='1234567', real_mode=True, name='Aigents', verbose=False):
+        self.name = name
+        self.verbose = verbose
+        self.base_url = base_url # Aigents Web API hosting URL
+        self.login_email = login_email # Aigents user identification by email 
+        self.secret_question = secret_question # Aigents prompt for password
+        self.secret_answer = secret_answer # Aigents password value
+        self.real_mode = real_mode # whether to connect to real Aigents server (True) or fake test oe (False) 
+        if self.verbose:
+            logger.info('Creating Aigents session')
+        #print(self)
+        self.create_session()
+    
+    def __del__(self):
+        if self.verbose:
+            logger.info('Closing Aigents session')
+        self.close_session()
 
-	def create_session(self):
-		self.session = requests.session()
-		if self.real_mode:
-			#TODO assertions
-			self.request('my email ' + self.login_email + '.')
-			self.request('my ' + self.secret_question + ' '  + self.secret_answer + '.')
-			self.request('my language english.')
-		else:
-			#TODO make sure if we can use only one of these
-			output = self.request('my name ' + self.login_email + ', surname ' + self.login_email + ', email ' + self.login_email + '.')
-			if output == 'What your secret question, secret answer?':
-				assert output == 'What your secret question, secret answer?', 'Expecting secret question, secret answer'
-				output = self.request('my secret question ' + self.secret_question + ', secret answer ' + self.secret_answer + '.')
-			assert output == 'What your ' + self.secret_question + '?', 'Expecting secret question'
-			output = self.request('my ' + self.secret_question + ' ' + self.secret_answer + '.')
-			assert output.split()[0] == 'Ok.', 'Expecting Ok'
+    def create_session(self):
+        self.session = requests.session()
+        if self.real_mode:
+            #TODO assertions
+            self.request('my email ' + self.login_email + '.')
+            self.request('my ' + self.secret_question + ' '  + self.secret_answer + '.')
+            self.request('my language english.')
+        else:
+            #TODO make sure if we can use only one of these
+            output = self.request('my name ' + self.login_email + ', surname ' + self.login_email + ', email ' + self.login_email + '.')
+            if output == 'What your secret question, secret answer?':
+                assert output == 'What your secret question, secret answer?', 'Expecting secret question, secret answer'
+                output = self.request('my secret question ' + self.secret_question + ', secret answer ' + self.secret_answer + '.')
+            assert output == 'What your ' + self.secret_question + '?', 'Expecting secret question'
+            output = self.request('my ' + self.secret_question + ' ' + self.secret_answer + '.')
+            assert output.split()[0] == 'Ok.', 'Expecting Ok'
 
-	def close_session(self):
-		if not self.real_mode:
-			output = self.request('Your trusts no ' + self.login_email + '.')
-			assert output.split()[0] == 'Ok.', 'Expecting Ok'
-			output = self.request('No name ' + self.login_email + '.');
-			assert output.split()[0] == 'Ok.', 'Expecting Ok'
-			output = self.request('No there times today.');
-			assert output.split()[0] == 'Ok.', 'Expecting Ok'
-		output = self.request('My logout.');
-		assert output.split()[0] == 'Ok.', 'Expecting Ok'
-			
-	def request(self,input):
-		if self.verbose:
-			logger.info(input)
-		url = self.base_url + '?' + urllib.parse.quote_plus(input)
-		try:
-			r = self.session.post(url)
-			if r is None or r.status_code != 200:
-				logger.error('request ' + url + ' error ' + str(r.status_code))
-				raise RuntimeError("Aigents - no response")
-		except Exception as e:
-			logger.error('request ' + url + ' ' + str(type(e)))
-			print('Specify proper url to Aigents server or run it locally, eg.')
-			print('java -cp ./bin/Aigents.jar:./bin/* net.webstructor.agent.Farm store path \'./al_test.txt\', cookie domain localtest.com, console off &')
-			print('or')
-			print('sh aigents_server_start.sh')
-			return 'No connection to Aigents, ' + str(type(e))
-		if self.verbose:
-			logger.info(r.text)
-		return r.text
-	
-	
+    def close_session(self):
+        if not self.real_mode:
+            output = self.request('Your trusts no ' + self.login_email + '.')
+            assert output.split()[0] == 'Ok.', 'Expecting Ok'
+            output = self.request('No name ' + self.login_email + '.');
+            assert output.split()[0] == 'Ok.', 'Expecting Ok'
+            output = self.request('No there times today.');
+            assert output.split()[0] == 'Ok.', 'Expecting Ok'
+        output = self.request('My logout.');
+        assert output.split()[0] == 'Ok.', 'Expecting Ok'
+            
+    def request(self,input):
+        if self.verbose:
+            logger.info(input)
+        url = self.base_url + '?' + urllib.parse.quote_plus(input)
+        try:
+            r = self.session.post(url)
+            if r is None or r.status_code != 200:
+                logger.error('request ' + url + ' error ' + str(r.status_code))
+                raise RuntimeError("Aigents - no response")
+        except Exception as e:
+            logger.error('request ' + url + ' ' + str(type(e)))
+            print('Specify proper url to Aigents server or run it locally, eg.')
+            print('java -cp ./bin/Aigents.jar:./bin/* net.webstructor.agent.Farm store path \'./al_test.txt\', cookie domain localtest.com, console off &')
+            print('or')
+            print('sh aigents_server_start.sh')
+            return 'No connection to Aigents, ' + str(type(e))
+        if self.verbose:
+            logger.info(r.text)
+        return r.text
+    
+    
 class AigentsSentiment():
 
     def __init__(self, api_url='http://localtest.com:1180/', debug=False):
@@ -146,7 +147,7 @@ class AigentsSentiment():
             if self.debug:
                 raise e
         return sen, pos, neg, con, wordcnt, itemcnt
-	
+    
 
 def token_valid(token):
     if len(token) < 1:
@@ -207,13 +208,13 @@ def build_ngrams(seq,N):
 
 def load_ngrams(file,debug=False):
     if file.lower().startswith('http'):
-    	lines = url_lines(file)
-    	if debug:
-    		print(lines[:100])
-    		print(len(lines))
+        lines = url_lines(file)
+        if debug:
+            print(lines[:100])
+            print(len(lines))
     else:
-	    with open(file) as f:
-	        lines = f.readlines()
+        with open(file) as f:
+            lines = f.readlines()
     ngrams = [tuple(l.split()) for l in lines if len(l) > 0]
     return set(ngrams)
 
@@ -228,56 +229,33 @@ def split_patterns(arg):
     return [split_pattern(a) for a in arg]
 assert str(split_patterns([('abcd',),('ef','gh'),('i','j')])) == "[('a', 'b', 'c', 'd'), ('e', 'f', 'g', 'h'), ('i', 'j')]"
 
-"""
-class TextMetrics(PygentsSentiment):
-    def __init__(self, metrics, metric_maximized=False, metric_logarithmic=True, tokenize_chars=False, debug=False):
-		self.metric_maximized = metric_maximized
-		self.metric_logarithmic = metric_logarithmic
-		self.tokenize_chars = tokenize_chars
-		self.gram_arity = 3
-#TODO don't use scrub!?
-#TODO
-		self.scrub = set(["-", "&","a", "an", "and", "because", "else", "or", "the", "in", "on", "at", "it", "is", "after", "are", "me",
-					"am", "i", "into", "its", "same", "with", "if", "most", "so", "thus", "hence", "how",
-					"as", "do", "what", "for", "to", "of", "over", "be", "will", "was", "were", "here", "there",
-					"you", "your", "our", "my", "her", "his", "just", "have", "but", "not", "that",
-					"their", "we", "by", "all", "any", "anything", "some", "something", "dont", "do", "does", "of", "they", "them",
-					"been", "even", "etc", "this", "that", "those", "these", "from", "he", "she",
-					"no", "yes", "own", "may", "mine", "me", "each", "can", "could", "would", "should", "since", "had", "has",
-					"when", "out", "also", "only", "about", "us", "via", "than", "then", "up", "who", "why", "which", "yet"])
-		self.punct = set(list("-—{([<})]>.,;:?$_.+!?*'\"\\/"))
-		
-		self.positives = self.to_set(positive_lexicon_file)
-		self.negatives = self.to_set(negative_lexicon_file)
-		#TODO use unsupervised tokenization!!!!
-		if self.tokenize_chars:
-			self.positives = split_patterns(self.positives)
-			self.negatives = split_patterns(self.negatives)
- """
 
-#TODO reuse the parent class above
-class PygentsSentiment():
-
-    def __init__(self, positive_lexicon_file, negative_lexicon_file, sentiment_maximized=False, sentiment_logarithmic=True, tokenize_chars=False, debug=False):
-        self.sentiment_maximized = sentiment_maximized
-        self.sentiment_logarithmic = sentiment_logarithmic
-        self.gram_arity = 3
-        self.scrub = set(["-", "&","a", "an", "and", "because", "else", "or", "the", "in", "on", "at", "it", "is", "after", "are", "me",
+#TODO move to data ("model") files
+scrub_en = ["-", "&","a", "an", "and", "because", "else", "or", "the", "in", "on", "at", "it", "is", "after", "are", "me",
                     "am", "i", "into", "its", "same", "with", "if", "most", "so", "thus", "hence", "how",
                     "as", "do", "what", "for", "to", "of", "over", "be", "will", "was", "were", "here", "there",
                     "you", "your", "our", "my", "her", "his", "just", "have", "but", "not", "that",
                     "their", "we", "by", "all", "any", "anything", "some", "something", "dont", "do", "does", "of", "they", "them",
                     "been", "even", "etc", "this", "that", "those", "these", "from", "he", "she",
                     "no", "yes", "own", "may", "mine", "me", "each", "can", "could", "would", "should", "since", "had", "has",
-                    "when", "out", "also", "only", "about", "us", "via", "than", "then", "up", "who", "why", "which", "yet"])
-        self.punct = set(list("-—{([<})]>.,;:?$_.+!?*'\"\\/"))
+                    "when", "out", "also", "only", "about", "us", "via", "than", "then", "up", "who", "why", "which", "yet"]
+punct = "-—{([<})]>.,;:?$_.+!?*'\"\\/"
+
+class PygentsSentiment():
+
+    def __init__(self, positive_lexicon_file, negative_lexicon_file, sentiment_maximized=False, sentiment_logarithmic=True, tokenize_chars=False, debug=False):
+        self.sentiment_maximized = sentiment_maximized
+        self.sentiment_logarithmic = sentiment_logarithmic
+        self.gram_arity = 3
+        self.scrub = set(scrub_en)
+        self.punct = set(list(punct))
         self.positives = self.to_set(positive_lexicon_file)
         self.negatives = self.to_set(negative_lexicon_file)
         self.tokenize_chars = tokenize_chars
         #TODO use unsupervised tokenization!!!!
-        if self.tokenize_chars:
-        	self.positives = split_patterns(self.positives)
-        	self.negatives = split_patterns(self.negatives)
+        if self.tokenize_chars:  # for chinese
+            self.positives = split_patterns(self.positives)
+            self.negatives = split_patterns(self.negatives)
 
     def to_set(self,arg):
         if type(arg) == set:
@@ -371,4 +349,87 @@ class PygentsSentiment():
             n = round(n,rounding)
             c = round(c,rounding)
         return p, -n, c
-	
+    
+
+class TextMetrics(PygentsSentiment):
+    def __init__(self, metrics, metric_logarithmic=True, tokenize_chars=False, scrub=[], debug=False):
+        self.metric_logarithmic = metric_logarithmic
+        self.tokenize_chars = tokenize_chars
+        self.scrub = scrub
+        self.punct = set(list(punct))
+        self.gram_arity = 1
+        self.metrics = {}
+        for metric in metrics:
+            #TODO use unsupervised tokenization!!!!
+            ngrams = self.to_set(metrics[metric])
+            if self.tokenize_chars: # for chinese
+                ngrams = split_patterns(ngrams)
+            self.metrics[metric] = ngrams
+            for ngram in ngrams: # get real arity on ngrams
+                l = len(ngram)
+                if self.gram_arity < l:
+                    self.gram_arity = l
+ 
+ 
+    def get_sentiment_words(self,input_text,lists=None,rounding=2,debug=False):
+        """
+        See original reference implementation:
+        https://github.com/aigents/aigents-java/blob/master/src/main/java/net/webstructor/data/LangPack.java#L355
+        """
+        if input_text is None or len(input_text) < 1:
+            return 0, 0, 0
+        seq = [*input_text] if self.tokenize_chars else tokenize_re(input_text) #TODO unsupervised tokenization
+        if debug:
+            print("<{}>".format(str(seq)))
+        if len(seq) < 1:
+            return 0, 0, 0
+        counts = {}
+        lists = {}
+        N = self.gram_arity
+        while N >= 1:
+            seq_ngrams = build_ngrams(seq,N)
+            if debug:
+                print(N,str(seq_ngrams))
+            if len(seq_ngrams) > 0:
+                for i in range(0,len(seq_ngrams)):
+                    w = seq_ngrams[i]
+                    if debug:
+                        print(N,w)
+                    if w is None or len(w) == 0 or (len(w) == 1 and (w[0] is None or self.is_scrub(w[0]))): #some may be None being consumed earlier
+                        i += 1
+                        continue
+                    found = False 
+                    
+                    for metric in self.metrics:
+                        ngrams = self.metrics[metric]
+                        if w in ngrams:                         
+                            dictcount(counts,metric,N) # p += N #weighted
+                            if not lists is None:
+                                if not metric in lists:
+                                    l = []
+                                    lists[metric] = l
+                                else:
+                                    l = lists[metric]
+                                l.append(w);
+                            found = True;
+
+#TODO what is that?
+                    if found:
+                        for Ni in range(0,N):
+                            seq[i + Ni] = None
+                        i += N
+                    else:
+                        i += 1  
+            N -= 1
+        lenseq = len(seq)
+        
+#TODO fix this
+        if self.metric_logarithmic:
+            for metric in counts:
+                counts[metric] = math.log10(1 + 100 * counts[metric] / lenseq)/2
+        else:
+            for metric in counts:
+                counts[metric] = counts[metric] / lenseq
+#TODO overall
+        return counts
+    
