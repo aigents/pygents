7311878eefa2682e060b020b65942ff08372362f
Anton Kolonin <kolonin@tornado.nsk.ru>
2025-03-02 23:28:42 +0700

    create separate folder for paper submission

---
+++ papers/distortions_binary_2025/plot.py
@@ -0,0 +1,250 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from text import *
+
+def matrix_plot(row_labels, col_labels, matrix, absmax, title = None, vmin = None, vmax = None):
+    plt.rcParams["figure.figsize"] = (20,len(row_labels)/4)
+    p = sns.heatmap(matrix, xticklabels=col_labels, yticklabels=row_labels, 
+                    vmin = -absmax if vmin is None else vmin, 
+                    vmax = absmax if vmax is None else vmax, 
+                    cmap='RdYlGn', annot=True)
+    if title is not None:
+        fontsize = 32 if len(title) < 50 else round(32 * 50 / len(title))
+        p.set_title(title,fontsize = fontsize)
+    plt.show()
+
+def plot_profile(df,text,h=3,title=None):
+    plt.rcParams["figure.figsize"] = (20,h)
+    p = df.plot.bar(x='char')
+    p = plt.xticks(rotation='horizontal',fontsize=1+round(60*20/len(text)))
+    if title is not None:
+        plt.title(title)
+
+def plot_hbar(df,labels,values,title=None):
+    plt.rcParams["figure.figsize"] = (20,round(len(df)/5))
+    p = df[[labels,values]].plot.barh(x=labels); p.invert_yaxis();
+    if title is not None:
+        plt.title(title)
+
+def plot_hbars(df,labels,values,title=None):
+    plt.rcParams["figure.figsize"] = (20,round(len(df)/5))
+    p = df[[labels]+values].plot.barh(x=labels); p.invert_yaxis();
+    if title is not None:
+        plt.title(title)
+
+def plot_bars(df,labels,values,title=None,fontsize=None):
+    plt.rcParams["figure.figsize"] = (20,3)
+    p = df[[labels]+values].plot.bar(x=labels);
+    p = plt.xticks(rotation='horizontal',fontsize = 1+round(60*20/len(df)) if fontsize is None else fontsize)
+    if title is not None:
+        fontsize = 32 if len(title) < 50 else round(32 * 50 / len(title))
+        plt.title(title,fontsize = fontsize)
+
+def plot_dict(dic,labels,values,title=None,head=None):
+    df = pd.DataFrame([(key, dic[key]) for key in dic],columns=[labels,values])
+    df.sort_values(values,ascending=False,inplace=True)
+    if head is not None:
+        df = df[:head]
+    plt.rcParams["figure.figsize"] = (20,round(len(df)/5))
+    p = df[[labels,values]].plot.barh(x=labels); p.invert_yaxis();
+    if title is not None:
+        plt.title(title)
+
+def plot_profile_probabilities(counters,text,max_n,plot=True,debug=False):
+    df = pd.DataFrame(profile_probabilities(counters,text,max_n,debug=debug),columns=['pos','char','p+','p-'])
+    #print(df)
+    df.set_index('pos',inplace=True)
+    if plot:
+        plt.rcParams["figure.figsize"] = (20,3)
+        plot_profile( df[['char','p+']],text)
+        plot_profile( df[['char','p-']],text)
+    return df
+
+def plot_profile_freedoms(model,text,max_n,plot=True,debug=False):
+    df = pd.DataFrame(profile_freedoms(model,text,max_n,debug=debug),columns=['pos','char','f+','f-'])
+    #print(df)
+    df.set_index('pos',inplace=True)
+    if plot:
+        plt.rcParams["figure.figsize"] = (20,3)
+        plot_profile( df[['char','f+']],text)
+        plot_profile( df[['char','f-']],text)
+    return df
+
+def plot_profile_avg_freedom(model,text,n_min,n_max,col):
+    sdf = None
+    for n in range(1,7):
+        plt.rcParams["figure.figsize"] = (20,2)
+        df = profile_freedoms_df(model,text,n,debug=False)
+        df[col] = df[col] / df[col].max()
+        plot_profile(df[['char',col]],text,title=str(n)+col)
+        if sdf is None:
+            sdf = df[['char',col]]
+        else:
+            sdf[col] = sdf[col] + df[col]
+
+    sdf[col] = sdf[col] / sdf[col].max()
+    plot_profile(sdf[['char',col]],text,title='1-7 '+col)
+
+
+def subplots_hist(df,labels,bins=100,fontsize=20):
+    fig, axs = plt.subplots(1, len(labels), figsize=(20, 4), sharey=True)
+    for ax, label in zip(axs,labels):
+        #p = ax.hist(df[[label]],bins=bins); p = ax.set_xlabel(label)
+        p = ax.hist(df[label],bins=bins); p = ax.set_xlabel(label,fontsize=fontsize)
+
+
+# graph plots
+
+# https://plotly.com/python/tree-plots/
+# https://igraph.org/python/doc/tutorial/tutorial.html
+
+from treelib import Node, Tree
+
+import igraph
+from igraph import Graph, EdgeSeq
+import plotly.graph_objects as go
+import math
+
+
+
+def make_annotations(pos, text, M, font_size=20, font_color='rgb(0,0,250)'):
+    L=len(pos)
+    if len(text)!=L:
+        raise ValueError('The lists pos and text must have the same len')
+    annotations = []
+    for k in range(L):
+        scaled_font_size = round(font_size/math.sqrt(len(text[k])))
+        if scaled_font_size < 2:
+            scaled_font_size = 2
+        annotations.append(
+            dict(
+                text=text[k], # or replace labels with a different list for the text within the circle
+                x=pos[k][0], y=2*M-pos[k][1],
+                xref='x1', yref='y1',
+                font=dict(color=font_color, size=scaled_font_size),
+                showarrow=False)
+        )
+    return annotations
+
+def igraph_draw(labels,edges,title):
+    #https://igraph.org/python/doc/tutorial/tutorial.html
+    G = Graph()
+    #G = Graph(directed=True) # TODO!!!???
+    nr_vertices = len(labels)
+    G.add_vertices(nr_vertices)
+    G.add_edges(edges)
+
+#    lay = G.layout('rt')
+    lay = G.layout('tree')
+
+    position = {k: lay[k] for k in range(nr_vertices)}
+    Y = [lay[k][1] for k in range(nr_vertices)]
+    M = max(Y)
+
+    es = EdgeSeq(G) # sequence of edges
+    E = [e.tuple for e in G.es] # list of edges
+
+    L = len(position)
+    Xn = [position[k][0] for k in range(L)]
+    Yn = [2*M-position[k][1] for k in range(L)]
+    Xe = []
+    Ye = []
+    for edge in E:
+        Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+        Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+    fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='bla',
+                  marker=dict(symbol='circle',#symbol='circle-dot',
+                                size=18, #18
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+    axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+    fig.update_layout(title=title,
+              annotations=make_annotations(position, labels, M),
+              font_size=12,
+              showlegend=False,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+    fig.show()
+
+
+
+def dict2graph(dictree):
+    parents_children_dict = {}
+    for child in dictree: 
+        dictcount(parents_children_dict,dictree[child])
+    parents_children_list = list(parents_children_dict)
+    parents_children_list.sort(key=lambda i: len(i), reverse=True)
+    nodes = []
+    edges = []
+    nodes2index = {}
+    for child in parents_children_list:
+        index = len(nodes)
+        nodes.append(child)
+        nodes2index[child] = index
+        if child in dictree:
+            edges.append((nodes2index[child],nodes2index[dictree[child]]))
+    for child in dictree: 
+        if not child in parents_children_list:
+            index = len(nodes)
+            nodes.append(child)
+            nodes2index[child] = index
+            edges.append((nodes2index[child],nodes2index[dictree[child]]))
+    return nodes, edges
+assert str(dict2graph({'c1':'c0','c2':'c0'})) == "(['c0', 'c1', 'c2'], [(1, 0), (2, 0)])"
+
+
+def dict2tree(dictree,debug=False):
+    parents_children_dict = {}
+    children = set()
+    for child in dictree: 
+        dictcount(parents_children_dict,dictree[child])
+        children.add(child)
+    parents_children_list = list(parents_children_dict)
+    parents_children_list.sort(key=lambda i: len(i), reverse=True)
+    if debug:
+        print(parents_children_list)
+    tree = Tree()
+    tree.create_node('', '') # root
+    for child in parents_children_list:
+        if debug:
+            print(child,'->',dictree[child] if child in dictree else '')
+        if not child in dictree:
+            tree.create_node(child, child, parent='')
+        else:
+            tree.create_node(child, child, parent=dictree[child])
+    for child in children - parents_children_dict.keys(): 
+        if debug:
+            print(child,'->',dictree[child] if child in dictree else '')
+        if not child in parents_children_list:
+            tree.create_node(child, child, parent=dictree[child])
+    return tree
+#print(dict2tree({'c1':'c0','c2':'c0','c3':'c2'}))
+assert(str(dict2tree({'c1':'c0','c2':'c0','c3':'c2'},False)).replace('\n','')) == "└── c0    ├── c1    └── c2        └── c3"
+
---
+++ papers/distortions_binary_2025/util.py
@@ -0,0 +1,383 @@
+import numpy as np
+import pickle
+import math
+from os.path import join
+from scipy.stats import entropy
+
+def vector_proximity(avector,bvector,threshold):
+    for a,b in zip(avector,bvector):
+        ab = a*b
+        if ab < 0: # check if sign is different
+            return False
+        if ab != 0:
+            if abs((a-b)/math.sqrt(ab)) > threshold:
+                #print(a,b,abs((a-b)/math.sqrt(ab)))
+                return False
+    return True
+assert not vector_proximity((10,-10,10),(10,10,10),0.1)
+assert vector_proximity((10,-10,10),(10,-10,10),0.1)
+assert vector_proximity((10,-10,10),(10,-10,10),0.1)
+assert not vector_proximity((10,-10,10),(19,-19,19),0.1)
+
+def dict_update(target,source):
+    for key in source:
+        if not key in target:
+            target[key] = source[key]
+        else:
+            target_value = target[key]
+            source_value = source[key]
+            if isinstance(source_value, dict):
+                assert isinstance(target_value, dict)
+                dict_update(target_value,source_value)
+            else:
+                assert (type(target_value) == int or float) and (type(source_value) == int or float)
+                target[key]= target_value + source_value
+    return target
+assert str(dict_update({'a':1},{'a':1,'b':20})) == "{'a': 2, 'b': 20}"
+assert str(dict_update({'a':1,'c':{'x':100}},{'a':1,'b':20,'c':{'x':300,'y':4000},'z':{'x':50000}})) == "{'a': 2, 'c': {'x': 400, 'y': 4000}, 'b': 20, 'z': {'x': 50000}}"
+
+
+def listofpairs_compress_with_loss(lst,threshold=0.01):
+    maxval = None
+    for i in lst:
+        if maxval is None or maxval < i[1]:
+            maxval = i[1]
+    newlist = []
+    minval = maxval * threshold
+    for i in lst:
+        if i[1] >= minval:
+            newlist.append(i)
+    return newlist
+assert str(listofpairs_compress_with_loss([('a',1000),('b',100),('c',10),('d',1)])) == "[('a', 1000), ('b', 100), ('c', 10)]"
+
+
+def dict_compress_with_loss(dic,threshold=0.01):
+    maxval = None
+    for d in dic:
+        v=dic[d]
+        if isinstance(v,dict):
+            dict_compress_with_loss(v,threshold) # recursion
+        else:
+            assert (type(v) == int or float)
+            if maxval is None or maxval < v:
+                maxval = v
+    if maxval is not None:
+        todo = []
+        minval = maxval * threshold
+        for d in dic:
+            if dic[d] < minval:
+                 todo.append(d)
+        for d in todo:
+            del dic[d]
+    return dic
+assert str(dict_compress_with_loss({'a':1000,'b':10,'c':1})) == "{'a': 1000, 'b': 10}"
+assert str(dict_compress_with_loss({'x':{'a':1000,'b':10,'c':1},'y':{'m':2000,'n':20,'o':2}})) == "{'x': {'a': 1000, 'b': 10}, 'y': {'m': 2000, 'n': 20}}"
+
+
+def dictdict_div_dict(num, den):
+    res = {}
+    for n in num:
+        if isinstance(num[n], dict):
+            res[n] = dictdict_div_dict(num[n], den)
+        else:
+            #print(n,num[n],den[n],type(num[n]),type(den[n]))
+            #k = float(num[n]) / den[n]
+            res[n] = float(num[n]) / den[n]
+    return res
+n = {"a":{"x":10,"y":15},"b":{"y":30,"z":40}}; d = {"x":20,"y":30,"z":40}; assert str(dictdict_div_dict(n,d))=="{'a': {'x': 0.5, 'y': 0.5}, 'b': {'y': 1.0, 'z': 1.0}}"
+
+
+def dictdict_mul_dictdict(num, den):
+    res = {}
+    for n in num:
+        if isinstance(num[n], dict):
+            #print(num[n], den[n])
+            res[n] = dictdict_mul_dictdict(num[n], den[n])
+            #print(res[n])
+        else:
+            res[n] = float(num[n]) * den[n]
+    return res
+n = {"a":{"x":10,"y":20},"b":{"x":20,"y":40}}; d = {"a":{"x":10,"y":5},"b":{"x":0.5,"y":0.25}}; assert str(dictdict_mul_dictdict(n,d))=="{'a': {'x': 100.0, 'y': 100.0}, 'b': {'x': 10.0, 'y': 10.0}}"
+
+
+def dict2listsorted(d):
+    return [(key, value) for key, value in sorted(d.items())]
+
+def dict_diff(a,b):
+    diff = {}
+    for key in set(a).union(set(b)):
+        if key in a and key in b:
+            delta = a[key] - b[key]
+            if delta != 0:
+                diff[key] = delta
+        elif key in a:
+            diff[key] = a[key]
+        elif key in b: 
+            diff[key] = - b[key]
+    return diff
+assert str(dict2listsorted(dict_diff({'a':1,'b':2,'c':3,'d':1},{'a':1,'b':3,'c':2,'x':1}))) == "[('b', -1), ('c', 1), ('d', 1), ('x', -1)]"
+
+def remove_all(collection,item):
+    while item in collection:
+        collection.remove(item)
+
+def dictcount(dic,arg,cnt=1):
+    if type(arg) == list:
+        #print(arg)
+        #print(dic)
+        for i in arg:
+            dictcount(dic,i,cnt)
+    elif arg in dic:
+        dic[arg] = dic[arg] + cnt
+    else:
+        dic[arg] = cnt
+
+def merge_two_dicts(x, y):
+    """Given two dictionaries, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+def countcount(dic,arg,subarg,cnt=1):
+    if arg in dic:
+        subdic = dic[arg]
+    else:
+        dic[arg] = subdic = {}
+    dictcount(subdic,subarg,cnt)
+
+def counters_init(max_n):  
+    return [{} for n in range(max_n)], [{} for n in range(max_n)], [{} for n in range(max_n)]
+
+def merge_dicts(dicts):
+    merged={}
+    for d in dicts:
+        merged.update(d)
+    return merged
+
+def count_subelements(element):
+    count = 0
+    if isinstance(element, list) or isinstance(element, tuple):
+        for child in element:
+            count += count_subelements(child)
+    elif isinstance(element, dict):
+        for key in element:
+            count += count_subelements(element[key])
+    else:
+        count = 1
+    return count 
+assert count_subelements(['1',2,[[3,'4',{'x':['5',6],'y':(7,'8')},{'z':{'p':9,'q':['10']}}]]]) == 10
+
+
+# Counting measures 
+
+#https://en.wikipedia.org/wiki/F-score
+def dict_precision(ground,guess):
+    true_positives = sum([min(guess[key],ground[key]) if key in ground else 0 for key in guess])
+    guess_positives = sum(guess.values())
+    return true_positives / guess_positives
+
+def dict_recall(ground,guess):
+    true_positives = sum([min(guess[key],ground[key]) if key in ground else 0 for key in guess])
+    ground_positives = sum(ground.values())
+    return true_positives / ground_positives
+
+def list2dict(lst):
+    dic = {}
+    for l in lst:
+        dictcount(dic,l)
+    return dic
+
+def listofpairs2dict(lst):
+    dic = {}
+    for i in lst:
+        dictcount(dic,i[0],i[1])
+    return dic
+
+def round_str(val,decimals=0):
+    if val == 0:
+        return '0.'+"".join('0'*decimals)  
+    s = str(round(val,decimals))
+    point = s.find(".")
+    zeros = decimals - (len(s) - point) + 1
+    #print(point,len(s),zeros)
+    return s + ('0'*zeros)
+
+def calc_f1(ground,guess):
+    if isinstance(ground,list):
+        ground = list2dict(ground)
+    if isinstance(guess,list):
+        guess = list2dict(guess)
+    true_positives = sum([min(guess[key],ground[key]) if key in ground else 0 for key in guess])
+    guess_positives = sum(guess.values())
+    ground_positives = sum(ground.values())
+    precision = true_positives / guess_positives
+    recall = true_positives / ground_positives
+    return 2 * precision * recall / (precision + recall) if precision > 0 or recall > 0 else 0 
+
+assert dict_precision({'a':1,'b':1},{'a':1,'c':1}) == 0.5
+assert dict_precision({'x':2},{'x':1}) == 1.0
+assert dict_precision({'x':2},{'x':4}) == 0.5
+assert dict_precision({'a':1,'b':1,'x':1},{'a':1,'c':1,'x':2}) == 0.5
+assert dict_recall({'a':1,'b':1},{'a':1,'c':1}) == 0.5
+assert dict_recall({'x':2},{'x':1}) == 0.5
+assert dict_recall({'x':2},{'x':4}) == 1.0
+assert dict_recall({'a':1,'x':1},{'a':1,'c':1,'x':2}) == 1.0
+assert dict_recall({'a':1,'b':1,'x':1},{'a':1,'c':1,'x':2}) == 2/3
+assert calc_f1({'a':1,'x':1},{'b':1,'y':1}) == 0
+assert calc_f1({'a':1,'b':1},{'a':1,'c':1}) == 0.5
+assert calc_f1({'a':1,'b':1,'c':1},{'a':1,'b':1}) == 0.8
+assert calc_f1({'a':1,'b':1},{'a':1,'b':1,'c':1}) == 0.8
+assert calc_f1({'a':1,'b':2},{'a':2,'b':4}) == 2/3
+assert calc_f1({'a':2,'b':4},{'a':1,'b':2}) == 2/3
+assert str(list2dict(['a','b','a','c','c','c'])) == "{'a': 2, 'b': 1, 'c': 3}"
+assert calc_f1(['ab','cd','ef','gh'],['ab','cd','ef','gh']) == 1.0
+assert calc_f1(['ab','cd','ef','gh'],['a','b','cd','e','f','gh']) == 0.4
+assert calc_f1(['ab','cd','ef','gh'],['abcd','ef','gh']) == 0.5714285714285715
+assert calc_f1(['ab','cd','ef','gh'],['abcd','ef']) == 1/3
+
+def calc_diff(ground,guess):
+    if isinstance(ground,list):
+        ground = list2dict(ground)
+    if isinstance(guess,list):
+        guess = list2dict(guess)
+    return dict_diff(ground,guess)
+
+def list2matrix(lst):
+    rows = 0
+    cols = 0
+    rows_dict = {}
+    cols_dict = {}
+    # create labels
+    for i in lst:
+        row = i[0]
+        col = i[1]
+        if not row in rows_dict:
+            rows_dict[row] = rows
+            rows += 1
+        if not col in cols_dict:
+            cols_dict[col] = cols
+            cols += 1
+    print(rows,cols)
+    print(rows_dict)
+    print(cols_dict)
+    matrix = np.zeros((rows,cols),dtype=float)
+    for i in lst:
+        row = i[0]
+        col = i[1]
+        val = i[2]
+        matrix[rows_dict[row],cols_dict[col]] = val
+    return sorted(set(rows_dict)), sorted(set(rows_dict)), matrix
+
+def list2matrix(lst):
+    rows = 0
+    cols = 0
+    rows_dict = {}
+    cols_dict = {}
+    rows_list = []
+    cols_list = []
+    # create labels
+    for i in lst:
+        row = str(i[0])
+        col = str(i[1])
+        if not row in rows_dict:
+            rows_dict[row] = rows
+            rows += 1
+            rows_list.append(row)
+        if not col in cols_dict:
+            cols_dict[col] = cols
+            cols += 1
+            cols_list.append(col)
+    #print(rows,cols)
+    #print(rows_dict)
+    #print(cols_dict)
+    matrix = np.zeros((rows,cols),dtype=float)
+    for i in lst:
+        row = str(i[0])
+        col = str(i[1])
+        val = i[2]
+        matrix[rows_dict[row],cols_dict[col]] = val
+    return rows_list, cols_list, matrix
+
+
+def context_save_load(context,context_name,folder='data/temp/'):
+    ##https://stackoverflow.com/questions/12544056/how-do-i-get-the-current-ipython-jupyter-notebook-name
+    pickle_name = join(folder,context_name)
+    if context is None:
+        context = pickle.load(open(pickle_name, 'rb'))
+    else:
+        pickle.dump(context, open(pickle_name, 'wb'), pickle.HIGHEST_PROTOCOL)
+    return context
+
+
+def evaluate_entropy(tokenized_texts,counts=None):
+    """
+    Normalized entropy 
+    """
+    lexicon = {}
+    tokens_count = 0
+    for i in range(len(tokenized_texts)):
+        tokenized_text = tokenized_texts[i]
+        count = 1 if counts is None else counts[i]
+        tokens_count += len(tokenized_text) * count
+        for token in tokenized_text:
+            dictcount(lexicon,token,count)
+    distribution = [lexicon[token]/tokens_count for token in lexicon]
+    e = entropy(distribution, base=2)
+    k = len(lexicon)
+    if k > 2:
+        e /= math.log2(k)
+    return e
+#print(evaluate_entropy([["aaaabbbbaaaabbbb"]]))
+#print(evaluate_entropy([["aaaabbbb"],["aaaabbbb"]]))
+#print(evaluate_entropy([["aaaa"],["bbbb"],["aaaa"],["bbbb"]]))
+#print(evaluate_entropy([["aa"],["aa"],["bb"],["bb"],["aa"],["aa"],["bb"],["bb"]]))
+#print(evaluate_entropy([["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"],["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"]]))
+assert str(evaluate_entropy([["aaaabbbbaaaabbbb"]])) == "0.0"
+assert str(evaluate_entropy([["aaaabbbb"],["aaaabbbb"]])) == "0.0"
+assert str(evaluate_entropy([["aaaa"],["bbbb"],["aaaa"],["bbbb"]])) == "1.0"
+assert str(evaluate_entropy([["aa"],["aa"],["bb"],["bb"],["aa"],["aa"],["bb"],["bb"]])) == "1.0"
+assert str(evaluate_entropy([["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"],["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"]])) == "1.0"
+
+
+def evaluate_anti_entropy(tokenized_texts,counts=None):
+    """
+    Normalized anti-entropy 
+    """
+    return 1.0 - evaluate_entropy(tokenized_texts,counts=counts)
+
+
+def evaluate_compression(texts,tokenized_texts,counts=None):
+    """
+    Coefficient of compression 
+    """
+    text_len = 0
+    tokenized_text_len = 0
+    tokens_count = 0
+    lexicon = {}
+    for i in range(len(texts)):
+        text_len += len(texts[i]) * (1 if counts is None else counts[i])
+    for i in range(len(tokenized_texts)):
+        tokenized_text = tokenized_texts[i]
+        count = (1 if counts is None else counts[i])
+        tokens_count += len(tokenized_text) * count
+        for token in tokenized_text:
+            tokenized_text_len += len(token) * count # just sanity checking
+            dictcount(lexicon,token,count)
+    tokens_len = 0
+    for token in lexicon:
+        tokens_len += len(token)
+    assert text_len == tokenized_text_len
+    return 1.0 - ((tokens_len + tokens_count) / text_len)
+#print(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaabbbbaaaabbbb"]]))
+#print(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaabbbb"],["aaaabbbb"]]))
+#print(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaa"],["bbbb"],["aaaa"],["bbbb"]]))
+#print(evaluate_compression(["aaaabbbbaaaabbbb"],[["aa"],["aa"],["bb"],["bb"],["aa"],["aa"],["bb"],["bb"]]))
+#print(evaluate_compression(["aaaabbbbaaaabbbb"],[["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"],["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"]]))
+assert str(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaabbbbaaaabbbb"]])) == "-0.0625"
+assert str(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaabbbb"],["aaaabbbb"]])) == "0.375"
+assert str(evaluate_compression(["aaaabbbbaaaabbbb"],[["aaaa"],["bbbb"],["aaaa"],["bbbb"]])) == "0.25"
+assert str(evaluate_compression(["aaaabbbbaaaabbbb"],[["aa"],["aa"],["bb"],["bb"],["aa"],["aa"],["bb"],["bb"]])) == "0.25"
+assert str(evaluate_compression(["aaaabbbbaaaabbbb"],[["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"],["a"],["a"],["a"],["a"],["b"],["b"],["b"],["b"]])) == "-0.125"
+
+
+
