b5941da9b0ecd98985d73929d2df16bbf5a5799a
akolonin <akolonin@gmail.com>
2025-06-20 12:05:54 +0700

    cosine similarity on splits

--- pygents/util.py
+++ pygents/util.py
@@ -187,6 +187,22 @@ def contains_seq(B,A):
 assert contains_seq(('L', 'R', 'L', 'L', 'L', 'R'),('L', 'R'))
 
 
+def sqrt_sum_squares_dict_values(dct):
+    return math.sqrt(sum(x*x for x in dct.values()))
+        
+def cosine_similarity(dict_1, dict_2):
+    intersecting_keys = list(dict_1.keys() & dict_2.keys())
+
+    List1 = list(dict_1[k] for k in intersecting_keys)
+    List2 = list(dict_2[k] for k in intersecting_keys)
+    
+    similarity = np.dot(List1,List2) / (sqrt_sum_squares_dict_values(dict_1) * sqrt_sum_squares_dict_values(dict_2))
+    return round(similarity, 2)
+
+assert cosine_similarity({"a": 1, "b": 2, "c": 3}, {"c": 5, "b": 4, "d": 6}) == 0.7
+assert cosine_similarity({"a": 1.0, "b": 0.5, "c": 0.1}, {"a": 1.0, "b": 0.4, "d": 0.1}) == 0.99
+
+
 # Counting measures 
 
 #https://en.wikipedia.org/wiki/F-score
